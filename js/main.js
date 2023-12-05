7240835fff4990508d50a475178eef6cdd63f449
dieromeo <diegoromeo99@icloud.com>
2023-12-05 14:30:52 +0100

    feat: dropdown menu

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
 'use strict';
+
+const { createApp } = Vue;
+
+createApp({
+    data() {
+        return {
+            show: 'more',
+        }
+    },
+    methods: {
+        dropdown() {
+            if (this.show === 'more') {
+                this.show = 'less';
+            }
+            else {
+                this.show = 'more';
+            }
+        }
+    }
+}).mount('#app');
